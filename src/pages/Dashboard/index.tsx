a92fc9439b667d5c29ac8071fffa795aad69b7b1
Herberticleber Carvalho Valadares <herbert.valadares@ssp.df.gov.br>
2023-03-07 11:57:01 -0300

    subindo

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-
-import { Title , Form} from "./styles";
+import { FiChevronRight } from "react-icons/fi";
+import { Title, Form, Repos } from "./styles";
 import logo from "../../assets/logo.svg";
 
 export const Dashboard: React.FunctionComponent = () => {
@@ -13,6 +13,20 @@ export const Dashboard: React.FunctionComponent = () => {
         <input placeholder="username/repository_name"></input>
         <button type="submit">Buscar</button>
       </Form>
+
+      <Repos>
+        <a href="/repositories">
+          <img
+            src="https://avatars.githubusercontent.com/u/53231911?v=4"
+            alt="Repositorio"
+          />
+          <div>
+            <strong>devkoalaa/GitCollection</strong>
+            <p>Repositorio de aprendizado de React</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+      </Repos>
     </>
   );
 };
